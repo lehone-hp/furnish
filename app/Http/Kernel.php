26dbadc705cedc8e2be772ee4e56a7d1f7188778
root <lehone4hope@gmail.com>
2018-12-06 15:57:03 +0100

    Fix bug with admin login. Changed import in kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\adminAuth;
+use App\Http\Middleware\AdminAuth;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'auth.admin' => AdminAuth::class
+        'auth.admin' => AdminAuth::class,
     ];
 
     /**
